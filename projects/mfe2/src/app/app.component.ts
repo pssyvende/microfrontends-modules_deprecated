624474d2a9f475103ec6a71e18dc51ae6f157c83
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 11:09:49 +0200

    6. change componentIDs in order to prevent collision

--- projects/mfe2/src/app/app.component.ts
+++ projects/mfe2/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-mfe2',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
