e3180cb5c43eacb7c54752fead3d5d84eeb3e486
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 13:14:40 +0200

    7. load microfrontends + fix

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe1',
+    loadChildren: () => loadRemoteModule({
+      type: 'manifest', // "script" | "module" | "manifest" | undefined
+      remoteName: 'mfe1',
+      exposedModule: './Module'
+    })
+    .then(m => m.AppModule)
+  },
+  {
+    path: 'mfe2',
+    loadChildren: () => loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'mfe2',
+      exposedModule: './Module'
+    })
+    .then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
