0d57253289e86a18091d2bc5cd9b67f0295c66e5
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 13:01:08 +0200

    4. configure remotes routing

--- projects/mfe2/src/app/app-routing.module.ts
+++ projects/mfe2/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
