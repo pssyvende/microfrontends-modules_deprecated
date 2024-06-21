1cf4c752b77f567ce4e62ab43cf97e5014c2ddbf
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 15:05:59 +0200

    10. build routes dynamically

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,27 +1,7 @@
-import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'mfe1',
-    loadChildren: () => loadRemoteModule({
-      type: 'manifest', // "script" | "module" | "manifest" | undefined
-      remoteName: 'mfe1',
-      exposedModule: './Module'
-    })
-    .then(m => m.AppModule)
-  },
-  {
-    path: 'mfe2',
-    loadChildren: () => loadRemoteModule({
-      type: 'manifest',
-      remoteName: 'mfe2',
-      exposedModule: './Module'
-    })
-    .then(m => m.AppModule)
-  }
-];
+export const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
