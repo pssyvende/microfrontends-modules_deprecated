e56532511c0c40a2f1097534365b30f4a71dfc68
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 11:04:55 +0200

    4. configure routing

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe1',
+    loadChildren: () => import('mfe1/Module').then(m => m.AppModule)
+  },
+  {
+    path: 'mfe2',
+    loadChildren: () => import('mfe2/Module').then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
