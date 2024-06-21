34f66082395dd5757e9f5ca4b788bcec847daf1d
Paulina Sikacka <paulina.sikacka@convista.com>
2024-06-21 15:14:51 +0200

    11. activate dynamic routing I

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { getManifest } from '@angular-architects/module-federation';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomManifest } from './utils/config';
+import { buildRoutes } from './utils/routes';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shell';
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const manifest = getManifest<CustomManifest>();
+
+    const routes = buildRoutes(manifest);
+    this.router.resetConfig(routes);
+  }
 }
